19758d1e167311c77da7c88543703bdc59a3dcb5
Your Name <you@example.com>
2021-12-04 22:55:14 +0800

    upload all new files

---
+++ todo-project/app/todo/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+
+class Todo(models.Model):
+    title = models.TextField(max_length=200)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return '%s: %s' % (self.id, self.title)
+
