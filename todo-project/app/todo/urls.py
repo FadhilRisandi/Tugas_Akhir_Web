6a699e717da7dd27430705dc552583be45e646cf
FadhilRisandi <72488757+FadhilRisandi@users.noreply.github.com>
2021-12-04 22:56:58 +0800

    Update urls.py

--- todo-project/app/todo/urls.py
+++ todo-project/app/todo/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-     path('done/', views.done, name='done'),
+    path('done/', views.done, name='done'),
     path('pending/', views.pending, name='pending'),
     path('delete_all/', views.delete_all, name='delete_all'),
     path('create/', views.create, name='create'),
